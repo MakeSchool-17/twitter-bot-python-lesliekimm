15d9739742a67c2d45b8de93c60a03bbf4742440
leslie kimm <leslie.kimm@gmail.com>
2015-09-24 15:03:11 -0700

    edited URL to read in

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,23 +1,24 @@
+# sys will allow access to command line arguments
+# random will allow to sample words from file
 import sys
 import random
 
-i_file = open('../../../../usr/share/dict/words', 'r')
-
+# read in number of words to randomly select
 num_of_words = int(sys.argv[1])
-#print("NUM OF WORDS:", num_of_words) # printout statement
-#print("FILE_TYPE", type(i_file))
+# hardcoded directory URL for dictionary file, open as read only
+i_file = open('/usr/share/dict/words', 'r')
 
+# read in each line of the file
 read_in_file = i_file.readlines()
-#print("READ_FILE_TYPE", type(read_in_file))
-
+# sample num_of_words words from read in file
 random_words = random.sample(read_in_file, num_of_words)
-#print("RANDOM INDICIES:", random_words)
 
-#for i in range(0, num_of_words):
-#    print(random_words[i], end=" ")
-
+# iterate through random_words list and print separated by spaces
 for i in range(0, num_of_words):
+    # last character of each line read in is a newline so do not
+    # read in last character
     word_to_print = (random_words[i])[0:-1]
     print(word_to_print, end=" ")
 
+# print an extra line
 print()
