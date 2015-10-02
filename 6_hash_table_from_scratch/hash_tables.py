7297b1d77e10fdaa0f08ff2c8ad178b6c6f346d1
leslie kimm <leslie.kimm@gmail.com>
2015-10-02 12:20:56 -0700

    Comment and add tests for Node & LinkedList class

--- 6_hash_table_from_scratch/hash_tables.py
+++ 6_hash_table_from_scratch/hash_tables.py
@@ -1,71 +1,119 @@
-import hashlib
-
+# class Node create instances of objects with key, value and next variables
 class Node:
+    # instantiates a Node ojbect with default values of None for key, value
+    # and next members
     def __init__(self, key=None, value=None, next=None):
         self.key = key                  # word
         self.value = value              # frequency
         self.next = None
 
-    def set_key(self, key):
-        self.key = key
-        return
-
+    # set the value of a Node object's member value
     def set_value(self, value):
         self.value = value
         return
 
+    # return a Node object's value member
     def return_value(self):
         return self.value
 
+    # print Node with value first formatted to right align of 10 chars
+    # followed by the key
+    def print_node(self):
+        print('%10s' % self.value, '  ', self.key)
+        return
 
+
+# class Linked Lisk create instances of Linked Lists of Node objects with
+# head and size variables.
 class LinkedList:
-    def __init__(self, head=None, size=None):
-        self.head = None
-        self.size = 0
+    # instantiates a LinkedList object with default value of None for head
+    # and size 0
+    def __init__(self, head=None, size=0):
+        self.head = None        # reference to first Node in LinkedList
+        self.size = 0           # number of Nodes in LinkedList
 
+    # inserts a Node object at the head of the LinkedList with key and value
+    # members of Node set to parameters passed in
     def insert(self, key, value):
+        # create a reference to the head
+        temp = self.head
+        # create new Node object
         new_node = Node(key, value)
 
-        if self.head is None:
+        # if the current LinkedList is empty, set head to new_node
+        if self.is_empty():
             self.head = new_node
+        # if current LinkedList is not empty, insert node at the beginning
+        # of LinkedList
         else:
-            new_node.next = self.head
+            new_node.next = temp
             self.head = new_node
+        # increment size of LinkedList
         self.size += 1
         return
 
-    # searches list for a key and returns pointer to the Node
+    # searches LinkedList for key value passed in as paraemter and returns
+    # a pointer to the corresponding Node
     def search(self, key):
+        # create a reference to the head
         temp = self.head
-        while temp.next is not None:
-            if temp.key is key:
-                return temp
-            else:
-                temp = temp.next
-        if temp.key is key:
-            return temp
-        return
 
-    def update_value(self, key, new_value):
-        temp = self.head
-        for i in range(0, self.size):
-            if temp is not None:
+        # if LinkedList is empty, print error message and return None
+        if self.is_empty():
+            print('ERROR: In LINKEDLIST class: There is nothing in this list.')
+            return None
+        # if LinkedList is not empty, search for key and return reference
+        else:
+            # traverse through list
+            while temp is not None:
+                # when key is found, return the reference to the Node object
                 if temp.key is key:
-                    temp.set_value(new_value)
-                    break
+                    return temp
+                # if key does not match, set temp to next Node
                 else:
                     temp = temp.next
-        if temp is None:
-            print('WARNING: In LINKEDLIST class: Key not found. Nothing ' +
-                  'to update.')
+            # if key was not found in LinkedList, print error message
+            if temp is None:
+                print('ERROR: In LINKEDLIST class: ' + key + ' not found. ' +
+                      'Nothing to find.')
+        return None
+
+    # search for key passed in as parameter and assign new_value
+    def update_value(self, key, new_value):
+        # if LinkedList is empty, print error message
+        if self.is_empty():
+            print('ERROR: In LINKEDLIST class: There is nothing in this list.')
+            return
+        # if LinkedList is not empty, search for the key and update the value
+        else:
+            # search for key and create reference to the Node
+            reference = self.search(key)
+            # if key was found, set the Node's value to the new_value
+            if reference is not None:
+                reference.set_value(new_value)
+            # if key is not found, print error message
+            else:
+                print('ERROR: In LINKEDLIST class: ' + key + ' not found. ' +
+                      'Nothing to update.')
         return
 
-    # print the list, frequency followed by the word
+    # returns True if LinkedList is empty and False if not empty
+    def is_empty(self):
+        return self.head is None
+
+    # print the LinkedList
     def print_list(self):
+        # create a reference to the head
         temp = self.head
-        while temp is not None:
-            print('%10s' % temp.value, '  ', temp.key)
-            temp = temp.next
+
+        # if LinkedList is emmpty, print warning
+        if self.is_empty():
+            print('WARNING: In LINKEDLIST class. Nothing to print.')
+        # if LinkedList is not empty, traverse through list and print node
+        else:
+            while temp is not None:
+                temp.print_node()
+                temp = temp.next
         return
 
 
@@ -95,53 +143,73 @@ class HashTable:
             node_to_update.set_value(new_value)
 
 
-print('creating hash table')
-myHT = HashTable(10)
-for i in range(0, myHT.size):
-    print(type(myHT.buckets_list[i]))
-myHT.insert('green', 2)
-myHT.insert('blue', 12)
+if __name__ == '__main__':
+    def test_node():
+        # test Node class
+        print('Testing Node class:')
+        head = Node('head', 10)
+        arm = Node('arm', 2)
+        leg = Node('leg', 21)
 
-for i in range(0, 10):
-    if myHT.buckets_list[i].head is not None:
-        myHT.buckets_list[i].print_list()
-    else:
-        print('empty bucket')
+        head.print_node()
+        arm.print_node()
+        leg.print_node()
 
-myHT.update_value('blue', 5)
-myHT.update_value('yellow', 7)
+        head.set_value(7)
+        arm.set_value(3)
+        leg.set_value(13)
 
-for i in range(0, 10):
-    if myHT.buckets_list[i].head is not None:
-        myHT.buckets_list[i].print_list()
-    else:
-        print('empty bucket')
+        print('Updated Nodes:')
+        head.print_node()
+        arm.print_node()
+        leg.print_node()
 
-# testing
-myLL = LinkedList()
-myLL.insert('hand', 13)
-myLL.insert('bye', 1)
-myLL.insert('hello', 12)
-myLL.insert('face', 20)
-print(myLL.size)
+        print('Testing return_value function:')
+        print('head', head.return_value())
+        print('arm', arm.return_value())
+        print('leg', leg.return_value())
+        return
 
-temp = myLL.head
-for i in range(0, myLL.size):
-    print(temp.key, temp.value)
-    temp = temp.next
+    def test_linked_list():
+        # test LinkedList class
+        print('Testing LinkedList class:')
+        body = LinkedList()
 
-myLL.update_value('hello', 14)
-myLL.update_value('booger', 2)
+        print('Testing is_empty function:')
+        if body.is_empty():
+            print('is_empty() works!')          # should print
 
-print('after update')
-temp = myLL.head
-for i in range(0, myLL.size):
-    print(temp.key, temp.value)
-    temp = temp.next
+        print('Testing print_list function for empty LinkedList:')
+        body.print_list()                       # should print error message
 
-tempA = myLL.search('face')
-print('testing search', tempA.key, tempA.value)
+        body.insert('head', 14)
+        body.insert('arm', 6)
+        body.insert('leg', 2)
 
-# set a key
-# return list of all keys
-# return list of all values
+        print('Testing insert and print_list function:')
+        body.print_list()                       # should print 2, 6 14
+
+        print('Testing search function:')
+        reference_pointer1 = body.search('head')
+        reference_pointer2 = body.search('leg')
+        reference_pointer1.set_value(1)
+        reference_pointer2.set_value(12)
+        body.print_list()                       # should print 12, 6, 1
+
+        print('Testing search function for key not in LinkedList:')
+        reference_pointer3 = body.search('chest')       # should print error
+
+        print('Testing update_value function:')
+        body.update_value('leg', 20)
+        body.update_value('head', 33)
+        body.print_list()                       # should print 20, 6, 33
+
+        print('Testing update_value function for key not in LinkedList:')
+        body.update_value('thigh', 2)           # should print error
+        return
+
+    def test_hash_tables():
+        return
+
+    # test_node()
+    test_linked_list()
