06037571ad7486d5d60ef526876c5c955240bebf
leslie kimm <leslie.kimm@gmail.com>
2015-10-06 14:55:07 -0700

    Add test text file

--- 9_tokenization/tokenization.py
+++ 9_tokenization/tokenization.py
@@ -1,41 +1,35 @@
 import sys
 import re
+# import random
 
 
-# initialize source_text to open corpus.txt
-source_text = open('the_hobbit.txt')
-# if filename was passed through, open text file passed through as CL arg
-if (len(sys.argv) > 1):
-    source_text = open(sys.argv[1])
+SOURCE_TEXT = open('the_hobbit.txt')
+
+# if the name of a text file is passed in as command line argument, set
+# SOURCE_TEXT to argument
+if len(sys.argv) > 1:
+    SOURCE_TEXT = open(sys.argv[1])
 
 
-# reads in each word from source_text & returns list of strings
-def parse_text(source_text):
-    # read file and append each word to list_of_all
+# take an opened file as an argument and read the content, split the words
+# and strip appropriate punctuation
+def tokenization(source_text):
     read_file = source_text.read()
-    list_of_all = re.split('\s+', read_file)
+    words = re.split('\s|[!?.,;()"_:]+|', read_file)
 
-    # initialize empty array for stripped words
-    filtered_words = []
+    tokens = []
 
-    # iterate through list_of_all and keep punctuation/char case - this is in
-    # order to keep track of words that are more likely to occur at the
-    # beginning or end of sentence
-    for i in range(0, len(list_of_all)):
-        # get word of current index
-        current_word = list_of_all[i]
-
-        # if reading in an empty string, ignore and do not add to list
-        if len(current_word) == 0:
+    for i in range(0, len(words)):
+        current_word = words[i]
+        if len(current_word) < 1:
             continue
-        # otherwise, add current_word to list
         else:
-            filtered_words.append(current_word)
-    return filtered_words
+            tokens.append(current_word)
 
+    return tokens
 
-if __name__ == "__main__":
-    def test():
-        returned_list = parse_text(source_text)
-        for i in range(0, len(returned_list)):
-            print(returned_list[i])
+if __name__ == '__main__':
+    tokens = tokenization(SOURCE_TEXT)
+
+    for i in range(0, len(tokens)):
+        print(tokens[i])
